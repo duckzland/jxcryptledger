6fbc0d4ab246cf8947ee69a25edebe8f5b40e23c
jason.xie@victheme.com <jason.xie@victheme.com>
2026-02-28 07:54:51 +0800

    added github action

---
+++ build-debian.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# 1. Build the Flutter binary
+flutter build linux --release
+
+# 2. Setup Variables
+APP_NAME="jxcryptledger"
+VERSION="1.0.0"
+DEB_DIR="build/debian_tmp"
+BUNDLE_DIR="build/linux/x64/release/bundle"
+
+# 3. Create the Debian structure
+rm -rf $DEB_DIR
+mkdir -p $DEB_DIR/usr/bin/$APP_NAME-data
+mkdir -p $DEB_DIR/usr/share/applications
+mkdir -p $DEB_DIR/usr/share/icons/hicolor/512x512/apps
+mkdir -p $DEB_DIR/DEBIAN
+
+# 4. Copy Flutter bundle to /usr/bin/
+cp -r $BUNDLE_DIR/* $DEB_DIR/usr/bin/
+
+# 5. Copy Icon to System Icons
+cp assets/icon.png $DEB_DIR/usr/share/icons/hicolor/512x512/apps/$APP_NAME.png
+
+# 6. Create Desktop Entry (Start Menu)
+cat <<EOF > $DEB_DIR/usr/share/applications/$APP_NAME.desktop
+[Desktop Entry]
+Version: $VERSION
+Name=JxCryptLedger
+GenericName=JxCryptLedger
+Comment=JxCryptLedger Transaction Manager
+Exec=/usr/bin/jxcryptledger
+Icon=$APP_NAME
+Type=Application
+Categories=Utility;
+Terminal=false
+EOF
+
+# 7. Create the Control File (Hardcoded to fix the 'diversion' bug)
+cat <<EOF > $DEB_DIR/DEBIAN/control
+Package: $APP_NAME
+Version: $VERSION
+Architecture: amd64
+Maintainer: Jason Xie <jason.xie@victheme.com>
+Depends: libgtk-3-0, libglib2.0-0, libjpeg-turbo8
+Description: JxCryptLedger Transaction Manager
+EOF
+
+# 8. Set Permissions
+chmod -R 755 $DEB_DIR/DEBIAN
+chmod +x $DEB_DIR/usr/bin/$APP_NAME
+
+# 9. Build the .deb
+dpkg-deb --build $DEB_DIR build/${APP_NAME}_${VERSION}_amd64.deb
+
+echo "Success! Created: build/${APP_NAME}_${VERSION}_amd64.deb"
