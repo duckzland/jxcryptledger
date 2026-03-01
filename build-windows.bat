7198171dfc3e63fa4463a47c53adbabae9e8d4f2
duckz <jason.xie@victheme.com>
2026-03-01 09:35:04 +0800

    Updated windows build script to parse version.txt for simple versioning

--- build-windows.bat
+++ build-windows.bat
@@ -1,23 +1,39 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: [0/3] Read version from file
+if not exist version.txt (
+    echo Error: version.txt not found!
+    pause
+    exit /b
+)
+set /p FULL_VERSION=<version.txt
+
+:: Optional: Split 1.2.3.4 into Name (1.2.3) and Number (4)
+:: This assumes your version.txt is always in the format X.X.X.X
+for /f "tokens=1,2,3,4 delims=." %%a in ("%FULL_VERSION%") do (
+    set BUILD_NAME=%%a.%%b.%%c
+    set BUILD_NUMBER=%%d
+)
+
+echo Building Version: %FULL_VERSION% (Name: %BUILD_NAME%, Number: %BUILD_NUMBER%)
 
 echo [1/3] Cleaning and Fetching...
 call flutter clean
-call flutter pub get
+:: call flutter pub get
 
 echo [2/3] Building Windows & MSIX...
-:: We build the executable first
-call flutter build windows --release
-:: Then we wrap it into the single .msix installer
-call dart run msix:create --build-windows false --install-certificate false
+:: Use the split variables for the executable
+call flutter build windows --release --build-name=%BUILD_NAME% --build-number=%BUILD_NUMBER%
+
+:: Use the full 4-digit version for the MSIX installer
+call dart run msix:create --version %FULL_VERSION% --build-windows false --install-certificate false
 
 echo [3/3] Pooling Installer...
-:: Create the build folder if it doesn't exist (shared with Linux)
 if not exist build mkdir build
-
-:: Copy the single installer file to the root build/ folder
 copy /Y "build\windows\x64\runner\Release\*.msix" "build\"
 
 echo ---------------------------------------
-echo Done! Windows Installer is in: build\
+echo Done! Version %FULL_VERSION% is in: build\
 echo ---------------------------------------
-pause
+:: pause
