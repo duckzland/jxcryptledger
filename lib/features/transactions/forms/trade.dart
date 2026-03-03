34d3b583f07e5d5c6ed63acbd20a80f6a242b867
duckz <jason.xie@victheme.com>
2026-03-04 07:39:43 +0800

    force all validation at transaction repo

--- lib/features/transactions/forms/trade.dart
+++ lib/features/transactions/forms/trade.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../../core/locator.dart';
-import '../../../core/log.dart';
 import '../../../core/utils.dart';
 import '../../../widgets/button.dart';
 import '../../../widgets/fields/amount.dart';
