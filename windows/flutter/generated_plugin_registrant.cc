88ebd362e8d8959d1add6e5b03c2353a7af42d3d
duckz <jason.xie@victheme.com>
2026-03-07 06:56:08 +0800

    Revamped notification to use a single package

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
-#include <win_toast/win_toast_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-  WinToastPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WinToastPlugin"));
 }
