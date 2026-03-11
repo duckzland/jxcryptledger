090159873033ff336f2654a9380a566d19aa819c
duckz <jason.xie@victheme.com>
2026-03-11 16:45:02 +0800

    Refactored Watcherboard to own the page now

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -3,7 +3,7 @@ import 'package:go_router/go_router.dart';
 
 import '../app/layout.dart';
 import '../features/settings/page.dart';
-import '../features/watchboard/panels/page.dart';
+import '../features/watchboard/page.dart';
 import '../features/tools/page.dart';
 import '../features/transactions/page.dart';
 import '../features/unlock/controller.dart';
@@ -44,7 +44,7 @@ class AppRouter {
           GoRoute(path: "/", redirect: (context, state) => "/transactions"),
           GoRoute(path: "/transactions", builder: (context, state) => const TransactionsPage()),
 
-          GoRoute(path: "/watchboard", builder: (context, state) => const PanelsPage()),
+          GoRoute(path: "/watchboard", builder: (context, state) => const WatchboardPage()),
           GoRoute(path: "/watchers", builder: (context, state) => const WatchersPage()),
           GoRoute(path: "/tools", builder: (context, state) => const ToolsPage()),
           GoRoute(path: "/settings", builder: (context, state) => const SettingsPage()),
