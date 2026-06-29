fc1c70aed41d7bf79da64aa01c969c5b54117330
jason.xie@victheme.com <jason.xie@victheme.com>
2026-06-29 15:01:02 +0800

    fix run-linux.sh

--- run-linux.sh
+++ run-linux.sh
@@ -3,4 +3,4 @@
 echo -e "\033[36mLaunching Flutter App on Windows with Development Configuration...\033[0m"
 
 # Execute flutter run and safely pass the argument down to the native binary
-flutter run -d windows --dart-entrypoint-args "--development"
+flutter run -d linux --dart-entrypoint-args "--development"
