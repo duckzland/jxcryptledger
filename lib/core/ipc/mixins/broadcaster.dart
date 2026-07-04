ac6989a46f2dbd8f632b88ecd33e4505da6c943b
duckz <jason.xie@victheme.com>
2026-07-04 21:57:27 +0800

    Moving ipc related mixins to ipc folder

---
+++ lib/core/ipc/mixins/broadcaster.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+import '../action.dart';
+import '../../mode.dart';
+import '../client.dart';
+import '../event.dart';
+import '../server.dart';
+import '../../runtime/locator.dart';
+
+mixin CoreIpcMixinsBroadcaster {
+  StreamSubscription? _broadcaster;
+
+  CoreIpcClient get ipcClient => locator<CoreIpcClient>();
+  CoreIpcServer get ipcServer => locator<CoreIpcServer>();
+
+  bool isBroadcastable = CoreMode.isServer;
+
+  void broadcasterAction(CoreIpcBroadcastEvent event) {}
+
+  void broadcasterListen() {
+    _broadcaster = ipcClient.onBroadcast.listen((event) {
+      broadcasterAction(event);
+    });
+  }
+
+  Future<void> broadcasterSend({required CoreIpcAction op, required String action, dynamic key, dynamic payload}) async {
+    await ipcClient.send(op: op, action: action, key: key, payload: payload);
+  }
+
+  bool broadcasterEmit(CoreIpcAction op, String? action, String? key, Uint8List? payload, {Socket? exclude}) {
+    if (!isBroadcastable) return false;
+    ipcServer.broadcast(op, action ?? "", key ?? "", payload ?? Uint8List(0), exclude: exclude);
+    return true;
+  }
+
+  void broadcasterDispose() {
+    _broadcaster?.cancel();
+  }
+}
